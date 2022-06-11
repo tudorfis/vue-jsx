ac67adb52bbfc407a540bc78f5c4b44676d6dbd8
tudorfis <tudor.fis@gmail.com>
2022-06-11 19:44:52 +0300

    changes

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  setup() {
+    const items = [ 'a', 'b', 'c' ]
+    
+    return () => (
+        <div>
+          {items.map(item => (
+            <strong>{item}</strong>
+          ))}
+        </div>
+    )
+  }
+}
+</script>
